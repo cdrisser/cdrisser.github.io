0e0f9659b97008e1e187e488a20dc9c147650bb6
cdrisser <cdrisser@asu.edu>
2020-11-17 15:49:26 -0700

    updated files to reflect skills of resume

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,16 @@
 $(document).ready(()=>{
+$(".resumedetails").hide();
 	$(".resume").click(()=>{
-		
-	})
+		$(".resume").hide();
+        $(".resumedetails").show().animate({
+      left: '25%',
+      opacity: '1',
+      width: '100vh',
+      height: '100vh'
+    },1500);
+	});
+$(".close-resume").click(()=>{
+    $(".resumedetails").hide();
+    $(".resume").show();
 })
+});
