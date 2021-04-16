58263f6e0666e1c52919abb6e684c38fe4c5e92d
cdrisser <cdrisser@asu.edu>
2021-04-16 15:18:29 -0700

    added animation to tool bar

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,15 @@
 function scrollToSection(goToSection){
        console.log(goToSection); document.getElementById(goToSection).scrollIntoView({behavior:'smooth', block:'start'})
         }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    console.log('here')
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("nav-header").classList.add('animate__animated', 'animate__bounceOutUp');
+  } else {
+    document.getElementById("nav-header").classList.remove('animate__animated', 'animate__bounceOutUp' );
+  
+}
+}
