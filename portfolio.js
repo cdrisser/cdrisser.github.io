ac1841662afb597a92cd6297d5856547696d5a3d
cdrisser <cdrisser@asu.edu>
2021-04-19 14:07:19 -0700

    fixed bug that was causing side drawer to only work once

--- portfolio.js
+++ portfolio.js
@@ -1,47 +1,60 @@
 const closeEle= document.querySelector('.sidedrawer-close');
-const ele =  document.getElementById('sidedrawer');
+ const ele1 =  document.getElementById('sidedrawer');
 const hamBut = document.querySelector('.hamburger-button');
 
 const openSideDrawer = ()=>{
-    
+   
     document.querySelector('#overlay').style.display = 'block';
     
-    hamBut.classList.add("animate__animated","animate__rotateOut", "animate__fast");
+    hamBut.classList.add("animate__animated","animate__rotateOut", "animate__faster");
     
-    ele.style.display = 'flex';
-    ele.classList.add("animate__animated","animate__slideInRight", "animate__faster");
-    closeEle.classList.add("animate__animated","animate__rotateIn", "animate__slower");
+    ele1.style.display = 'flex';
+    ele1.classList.add("animate__animated","animate__slideInRight");
+    closeEle.classList.add("animate__animated","animate__rotateIn", "animate__faster");
     closeEle.style.display='flex';
     
-    
-    closeEle.addEventListener('animationend',()=>{
+        ele1.addEventListener('animationend',()=>{
+            console.log("here1")
         hamBut.style.display="none";
-        hamBut.classList.remove("animate__animated","animate__rotateOut", "animate__fast");
-        ele.classList.remove("animate__animated","animate__slideInRight", "animate__faster");
-        closeEle.classList.remove("animate__animated","animate__rotateIn", "animate__slower");
-        })
-    
-}
-const closeSideDrawer = ()=>{
-    
-    document.querySelector('#overlay').style.display = 'none';
-    
-    closeEle.classList.add("animate__animated","animate__rotateOut", "animate__slower");
-    
-    ele.classList.add("animate__animated","animate__slideOutRight", "animate__faster");
-    
-    hamBut.classList.add("animate__animated","animate__rotateIn", "animate__slower");
-    hamBut.style.display='flex';
-    
-    ele.addEventListener('animationend',()=>{
-        ele.style.display="none";
-        closeEle.style.display="none";
+        hamBut.classList.remove("animate__animated","animate__rotateOut", "animate__faster");
+        ele1.classList.remove("animate__animated","animate__slideInRight", "animate__faster");
+        closeEle.classList.remove("animate__animated","animate__rotateIn", "animate__faster");
+        },{once:true});
         
        
        
-        })
+    }
 
-}
+    
+    
+
+const closeSideDrawer = ()=>{
+    document.querySelector('#overlay').style.display = 'none';
+    
+    closeEle.classList.add("animate__animated","animate__rotateOut", "animate__faster");
+    
+    
+    ele1.classList.add("animate__animated","animate__slideOutRight");
+    hamBut.classList.add("animate__animated","animate__rotateIn", "animate__faster");
+    hamBut.style.display ='flex';
+    
+        ele1.addEventListener('animationend',()=>{
+            closeEle.style.display='none';
+        ele1.style.display = 'none';
+        
+        closeEle.style.display="none";
+         closeEle.classList.remove("animate__animated","animate__rotateOut", "animate__faster");
+        ele1.classList.remove("animate__animated","animate__slideOutRight", "animate__faster");
+        hamBut.classList.remove("animate__animated","animate__rotateIn", "animate__faster");
+        },{once:true})
+   
+      
+  
+         
+    }
+    
+
+
 
 
 function scrollToSection(goToSection){
